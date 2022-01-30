ead91bf32567eeb5cc8785d8261520a34fbcf135
lionh2ad <zlbearlz@naver.com>
2022-01-30 16:20:56 +0900

    fast_A_Plus_B_Update

---
+++ for_sentence/04_fast_A_Plus_B/04_fast_A_Plus_B/04_fast_A_Plus_B.c
@@ -0,0 +1,24 @@
+#include"04_fast_A_Plus_B.h"
+
+int main(void) {
+	int T = 0, A = 0, B = 0, count =0;
+	while (1) {
+		scanf("%d", &T);
+		if (0<T && T<1000000) {
+			break;
+		}
+	}
+	for (count =0;count <T;count++) {
+		while (1) {
+			scanf("%d %d", &A, &B);
+			if (1<=A && A<=1000) {
+				if (1<=B && B<=1000) {
+					break;
+				}
+			}
+		}
+		printf("%d", A+B);
+	}
+
+	return 0;
+}
