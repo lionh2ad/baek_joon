833a687ba33c7cdbfaeea1d243ad7809b7a572e9
lionh2ad <zlbearlz@naver.com>
2022-02-03 21:53:37 +0900

    plus_cycle_Update

--- while_sentence/03_plus_cycle/03_plus_cycle/03_plus_cycle.c
+++ while_sentence/03_plus_cycle/03_plus_cycle/03_plus_cycle.c
@@ -1,17 +1,33 @@
 #include"03_plus_cycle.h"
 
 int main(void) {
-	int N = 0, count = 0;
+	int first_N = 0, f_o_N = 0,f_t_n=0, count = 0;
+	int N = 0, one_N = 0, ten_N = 0;
 	while (1) {
 		while (1) {
-			scanf("%d", &N);
-			if (0<=N&&N<=99) {
+			scanf("%d", &first_N);
+			if (0 <= first_N && first_N <= 99) {
 				break;
 			}
 		}
-		for (count=0;count<4;count++) {
-
+		f_o_N = first_N % 10;
+		f_t_n = (first_N / 10) % 10;
+		if (f_o_N+f_t_n>=10) {
+			N = f_o_N * 10 + (f_o_N + f_t_n) % 10;
+		}else {
+			N = f_o_N * 10 + (f_o_N + f_t_n);
 		}
+		for (count = 1; first_N != N;count++) {
+			one_N = N % 10;
+			ten_N = (N / 10) % 10;
+			if (N >= 10) {
+				N = one_N * 10 + (one_N + ten_N)%10;
+			}else {
+				N = one_N * 10 + (one_N + ten_N);
+			}
+		}
+		printf("%d", count);
+		break;
 	}
 	return 0;
 }
