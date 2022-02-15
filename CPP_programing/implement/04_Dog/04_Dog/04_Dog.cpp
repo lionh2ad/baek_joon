6edaee9546295588e64b4ec5376b536fcdc89890
SUNGMIN-COMPUTE\WaveM <zlbearlz@naver.com>
2022-02-15 18:05:35 +0900

    CPP_04_Dog_Update

---
+++ CPP_programing/implement/04_Dog/04_Dog/04_Dog.cpp
@@ -0,0 +1,10 @@
+#include"04_Dog.h"
+
+int main(void) {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"\`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
