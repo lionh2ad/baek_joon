c25a5135b43bf3a05ee45badc88595f5e8ca51c5
lionh2ad <zlbearlz@naver.com>
2022-02-17 23:48:11 +0900

    02_A_minus_B

---
+++ CPP_programing/arithmetic_operation/02_A_minus_B/02_A_minus_B/02_A_minus_B.cpp
@@ -0,0 +1,16 @@
+#include"02_A_minus_B.h"
+
+int main(void) {
+	int A = 0, B = 0, result = 0;
+	while (1) {
+		cin >> A >> B;
+		if (0 < A && A < 10) {
+			if (0 < B && B < 10) {
+				break;
+			}
+		}
+	}
+	result = A - B;
+	cout << result << endl;
+	return 0;
+}
